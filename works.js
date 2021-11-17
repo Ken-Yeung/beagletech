9b6e755722e1a41f7ff09cccf819c7d58988cc6e
Yeung Chun Siu <yeungchunsiu@KENYCS-2.local>
2021-11-17 18:46:54 +0800

    restarting to work

--- works.js
+++ works.js
@@ -1,4 +1,26 @@
+class event_listen {
+    constructor(){
+        this.form = document.querySelectorAll(".form");
+
+        this.form_1_sumbit = document.getElementById("submit-step1");
+        // this.form_1 = document.getElementById("form")
+
+    }
+
+    all_forms(){
+        for(let i = 0; i < this.form.length; i++){
+            this.form[i].addEventListener("submit", (e)=>{
+                e.preventDefault();
+            });
+        }
+    }
+
+}
+
 (function(){
+    const listener = new event_listen();
+    listener.all_forms();
+
     console.log("Successfully imported and updated");
     document.getElementById("btn-start").addEventListener("click",(e)=>{
         document.getElementById("main-tab-1").click();
