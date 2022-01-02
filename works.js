cc9c9eb7d3a848c90d9f5ef30a093ffba36d937d
Yeung Chun Siu <yeungchunsiu@KENYCS-2.local>
2022-01-02 13:41:54 +0800

    auto update

--- works.js
+++ works.js
@@ -110,33 +110,69 @@ class event_listen {
                 const btn_id = e.target.id.split("-");
                 const btn_state = btn_id[0];
                 if(btn_state == "form"){
-                    try{
-                        let tab_id = (parseInt(btn_id[2].charAt(btn_id[2].length-1)) + 1).toString();
-                        document.getElementById(`main-tab-${tab_id}`).click();
-                    } catch (error){
-                        try {
-                            switch (btn_id[2]) {
-                                case "preview":
-                                    document.getElementById(`main-tab-${btn_id[2]}`).click();
-                                    break;
+                    switch (btn_id[2]) {
+                        case "step1":
+                            let ram_id = (parseInt(btn_id[2].charAt(5)) + 1).toString();
+                            document.getElementById(`main-tab-${ram_id}`).click();
+                            break;
+                        
+                        case "step2":
+                            let ram_id = (parseInt(btn_id[2].charAt(5)) + 1).toString();
+                            document.getElementById(`main-tab-${ram_id}`).click();
+                            break;
+                        
+                        case "step3":
+                            let ram_id = (parseInt(btn_id[2].charAt(5)) + 1).toString();
+                            document.getElementById(`main-tab-${ram_id}`).click();
+                            break;
 
-                                case "payment":
-                                    document.getElementById(`main-tab-${btn_id[2]}`).click();
-                                    break;
+                        case "step4":
+                            document.getElementById("main-tab-preview").click();
+                            break;
 
-                                case "final":
-                                    document.getElementById(`main-tab-${btn_id[2]}`).click();
-                                    break;
+                        case "preview":
+                            document.getElementById(`main-tab-${btn_id[2]}`).click();
+                            break;
 
-                                default:
-                                    console.log(err_msg);
-                                    break;
-                            }
-                        } catch (err){
+                        case "payment":
+                            document.getElementById(`main-tab-${btn_id[2]}`).click();
+                            break;
+
+                        case "final":
+                            document.getElementById(`main-tab-${btn_id[2]}`).click();
+                            break;
+
+                        default:
                             console.log(err_msg);
-                            // console.log(`ID: main-tab-${(parseInt(btn_id.charAt(btn_id.length-1)) + 1).toString()}`);
-                        }
+                            break;
                     }
+                    // try{
+                    //     let tab_id = (parseInt(btn_id[2].charAt(btn_id[2].length-1)) + 1).toString();
+                    //     document.getElementById(`main-tab-${tab_id}`).click();
+                    // } catch (error){
+                    //     try {
+                    //         switch (btn_id[2]) {
+                    //             case "preview":
+                    //                 document.getElementById(`main-tab-${btn_id[2]}`).click();
+                    //                 break;
+
+                    //             case "payment":
+                    //                 document.getElementById(`main-tab-${btn_id[2]}`).click();
+                    //                 break;
+
+                    //             case "final":
+                    //                 document.getElementById(`main-tab-${btn_id[2]}`).click();
+                    //                 break;
+
+                    //             default:
+                    //                 console.log(err_msg);
+                    //                 break;
+                    //         }
+                    //     } catch (err){
+                    //         console.log(err_msg);
+                    //         // console.log(`ID: main-tab-${(parseInt(btn_id.charAt(btn_id.length-1)) + 1).toString()}`);
+                    //     }
+                    // }
                 }
             });
         }
