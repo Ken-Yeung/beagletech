8ea53da99d964b00c0ce620a08b492a8c25172ed
kenycs <yeungchunsiu@gmail.com>
2022-01-11 09:59:12 +0800

    auto update

--- dark_mode.js
+++ dark_mode.js
@@ -1,52 +1,68 @@
-var dark_mode = (e)=>{
-    let classes = [
-        {
-            "mode": "dark-background",
-            "ids": [
-                "content_wrapper",
-                "mini_box_1",
-                "mini_box_2",
-                "mini_box_3",
-                "ts",
-                "ts_label",
-                "impact",
-                "impact_label",
-                "to",
-                "to_label",
-                "arg_selector_1",
-                "arg_selector_2",
-                "arg_selector_3",
-            ]
-        },
-        {
-            "mode": "light-dark-bg",
-            "ids": [
-                "nav",
-                "portal_progress",
-                "main-home-tutorial",
-                "main-home-about",
-                "main-home-showcase",
-            ]
-        },
-        {
-            "mode": "dark-words",
-            "ids": [
-                "btn-start",
-                "popup_back"
-            ]
-        }
-    ];
+class dark_theme{
+    constructor(){
+        this.classes = [
+            {
+                "mode": "dark-background",
+                "ids": [
+                    "content_wrapper",
+                    "mini_box_1",
+                    "mini_box_2",
+                    "mini_box_3",
+                    "ts",
+                    "ts_label",
+                    "impact",
+                    "impact_label",
+                    "to",
+                    "to_label",
+                    "arg_selector_1",
+                    "arg_selector_2",
+                    "arg_selector_3",
+                ]
+            },
+            {
+                "mode": "light-dark-bg",
+                "ids": [
+                    "nav",
+                    "portal_progress",
+                    "main-home-tutorial",
+                    "main-home-about",
+                    "main-home-showcase",
+                ]
+            },
+            {
+                "mode": "dark-words",
+                "ids": [
+                    "btn-start",
+                    "popup_back"
+                ]
+            }
+        ];
+    }
 
-    for(let i = 0; i < classes.length; i++){
-        for(let ii = 0; ii < classes[i].ids.length; ii++){
-            let mode_change = new class_worker(classes[i].ids[ii]);
-            mode_change.remove(classes[i].mode);
+    remove(){
+        for(let i = 0; i < this.classes.length; i++){
+            for(let ii = 0; ii < this.classes[i].ids.length; ii++){
+                let mode_change = new class_worker(this.classes[i].ids[ii]);
+                mode_change.remove(this.classes[i].mode);
+            }
         }
     }
 
-};
+    add(){
+        for(let i = 0; i < this.classes.length; i++){
+            for(let ii = 0; ii < this.classes[i].ids.length; ii++){
+                let mode_change = new class_worker(this.classes[i].ids[ii]);
+                mode_change.add(this.classes[i].mode);
+            }
+        }
+    }
+}
 
 (function(){
-    dark_mode();
-    // document.getElementById("changemode").addEventListener("toggle", dark_mode);
+    let dark_mode = new dark_theme();
+    dark_mode.remove();
+    document.getElementById("theme").addEventListener("click", (e)=>{
+        let theme_control = new dark_theme();
+        theme_control.add();
+    });
 })();
