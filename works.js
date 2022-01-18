79c7d5b2eded3b009d7d142f9c7aea9bab559b37
Ken-Yeung <yeungchunsiu@gmail.com>
2022-01-18 18:28:06 +0800

    auto update

--- works.js
+++ works.js
@@ -624,6 +624,11 @@ class event_listen {
     }
 
     init(){
+
+        document.querySelectorAll("#back").addEventListener("click", (e)=>{
+            history.back();
+        });
+
         this.start.addEventListener("click", (e)=>{
             push_history("1", "main-tab-1");
 
