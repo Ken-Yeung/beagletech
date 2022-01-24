d85008e84b1af822699962174cf59aca8099438a
Ken-Yeung <yeungchunsiu@gmail.com>
2022-01-25 01:39:56 +0800

    auto update

--- works.js
+++ works.js
@@ -895,7 +895,7 @@ class form_formation {
                 try {
                     document.getElementById("btn-start").click();
                 } catch (error){}
-            },999 + 666); // Auto Delay start if record found
+            },777); // Auto Delay start if record found
 
             document.getElementById("ts").value = data.topic.subject;
             document.getElementById("impact").value = data.topic.impact;
