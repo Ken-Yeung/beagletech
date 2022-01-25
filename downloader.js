a227a9ef8faedc9af4bac85c498437613d9ddc94
kenycs <yeungchunsiu@gmail.com>
2022-01-26 03:06:21 +0800

    auto update

--- downloader.js
+++ downloader.js
@@ -1,11 +1,14 @@
 (function(){
     const user_btn = document.getElementById("user_btn");
+    console.log("Hello World.");
     
     try {
         const typpo = new URLSearchParams(window.location.search).get('type');
         if (typpo=="user"){
             user_btn.click();
         }
+
+        // console.log("Hello World.");
     } catch(e){}
     
   })();
